c8124d6ba7a50b4eb9afc919d10aa758121286ff
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-06-12 11:52:00 +0900

    initial commit

---
+++ WatchLogger Watch App/LogDataCSVManager.swift
@@ -0,0 +1,51 @@
+//
+//  LogDataCSVManager.swift
+//  WatchLogger
+//
+//  Created by iwashita on 2023/06/12.
+//
+
+import Foundation
+
+class LogDataCSVManager {
+    private(set) var isRecording = false
+    private let headerText = "timestamp,attitudeX,attitudeY,attitudeZ,gyroX,gyroY,gyroZ,gravityX,gravityY,gravityZ,accX,accY,accZ"
+    private var recordText = ""
+    
+    var format = DateFormatter()
+    
+    
+    /// Initializer
+    init() {
+        format.dateFormat = "yyyyMMddHHmmssSSS"
+    }
+    
+    func startRecording(reset: Bool) {
+        if reset {
+            recordText = ""
+            recordText += headerText + "\n"
+        }
+        isRecording = true
+    }
+    
+    func stopRecording() {
+        isRecording = false
+    }
+    
+    func addRecordText(addText: String) {
+        recordText += addText + "\n"
+    }
+    
+    func saveSensorDataToCsv(fileName: String) throws {
+        let fileManager = FileManager.default
+        let docPath = NSHomeDirectory() + "/Documents"
+        let filePath = docPath + "\(fileName).csv"
+        let data = recordText.data(using: .utf8) ?? Data()
+        
+        if !fileManager.fileExists(atPath: filePath) {
+            fileManager.createFile(atPath: filePath, contents: data)
+        } else {
+            throw NSError(domain: "error", code: -1, userInfo: nil)
+        }
+    }
+}
---
+++ WatchLogger Watch App/SensorLogger.swift
@@ -0,0 +1,100 @@
+//
+//  LoggerManager.swift
+//  WatchLogger
+//
+//  Created by iwashita on 2023/06/12.
+//
+
+import Foundation
+import CoreMotion
+
+class SensorLogger: ObservableObject {
+    let motionManager = CMMotionManager()
+    let csvManager = LogDataCSVManager()
+    
+    var format = DateFormatter()
+    var intervalSeconds = 0.02
+    var attitude = SIMD3<Double>.zero
+    var gyro = SIMD3<Double>.zero
+    var gravity = SIMD3<Double>.zero
+    var acc = SIMD3<Double>.zero
+    
+    /// Start recording sensor data
+    /// - Parameter intervalSeconds: 記録周期
+    func startSensorUpdates(reset: Bool, intervalSeconds: Double = 0.02) {
+        csvManager.startRecording(reset: reset)
+        if motionManager.isDeviceMotionAvailable{
+            motionManager.deviceMotionUpdateInterval = intervalSeconds
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { (motion, error) in
+                self.getMotionData(deviceMotion: motion!)
+            })
+        }
+    }
+    
+    /// Stop or resume sensor data
+    func stopSensorUpdates() {
+        csvManager.stopRecording()
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.stopDeviceMotionUpdates()
+        }
+    }
+    
+    /// センサデータを書き込む
+    /// - Parameter deviceMotion: センサ
+    func getMotionData(deviceMotion: CMDeviceMotion) {
+        attitude.x = deviceMotion.attitude.pitch
+        attitude.y = deviceMotion.attitude.roll
+        attitude.z = deviceMotion.attitude.yaw
+        gyro.x = deviceMotion.rotationRate.x
+        gyro.y = deviceMotion.rotationRate.y
+        gyro.z = deviceMotion.rotationRate.z
+        gravity.x = deviceMotion.gravity.x
+        gravity.y = deviceMotion.gravity.y
+        gravity.z = deviceMotion.gravity.z
+        acc.x = deviceMotion.userAcceleration.x
+        acc.y = deviceMotion.userAcceleration.y
+        acc.z = deviceMotion.userAcceleration.z
+                
+        // record sensor data
+        if csvManager.isRecording {
+            format.dateFormat = "yyyy/MM/dd HH:mm:ss.SSS"
+            
+            var text = ""
+            text += format.string(from: Date()) + ","
+            //print(text)
+                        
+            text += String(attitude.x) + ","
+            text += String(attitude.y) + ","
+            text += String(attitude.z) + ","
+            text += String(gyro.x) + ","
+            text += String(gyro.y) + ","
+            text += String(gyro.z) + ","
+            text += String(gravity.x) + ","
+            text += String(gravity.y) + ","
+            text += String(gravity.z) + ","
+            text += String(acc.x) + ","
+            text += String(acc.y) + ","
+            text += String(acc.z)
+            
+            csvManager.addRecordText(addText: text)
+        }
+    }
+    
+    /// CSVに記録する
+    func saveAsCsv() async throws {
+        if csvManager.isRecording {
+            csvManager.stopRecording()
+            
+            // save csv file
+            format.dateFormat = "yyyy-MMdd-HHmmss"
+            let dateText = format.string(from: Date())
+            do {
+                try csvManager.saveSensorDataToCsv(fileName: "watchmotion_" + dateText)
+            } catch {
+                throw error
+            }
+        }
+    }
+
+}
